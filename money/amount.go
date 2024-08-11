6729c24871daec0a1ce6527d5cc17ca381d8f5f0
Adit0507 <adi05072002@gmail.com>
2024-08-11 17:28:19 +0530

    validating & implementing stringer interface

--- money/amount.go
+++ money/amount.go
@@ -8,9 +8,28 @@ type Amount struct {
 const ErrTooPrecise = Error("quantity is too precise")
 
 func NewAmount(quantity Decimal, currency Currency) (Amount, error) {
-	if quantity.precision > currency.precision {
+	switch {
+	case quantity.precision > currency.precision:
 		return Amount{}, ErrTooPrecise
+
+	case quantity.precision < currency.precision:
+		quantity.subunits *= pow10(currency.precision - quantity.precision)
+		quantity.precision = currency.precision
 	}
 
-	return Amount{quantity: quantity, currency: currency}, nil
+	return Amount{quantity: quantity, currency: currency}, nil}
+
+func (a Amount) String() string {
+	return a.quantity.String() + " " + a.currency.code
+}
+
+func (a Amount) validate() error {
+	switch {
+	case a.quantity.subunits > maxDecimal:
+		return ErrTooLarge
+	case a.quantity.precision > a.currency.precision:
+		return ErrTooPrecise
+	}
+
+	return nil
 }
