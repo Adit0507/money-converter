a8222c58824160f40fc56cf4a73593b2bf515548
Adit0507 <adi05072002@gmail.com>
2024-08-12 21:33:22 +0530

    done :fire:

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"moneyconverter/ecbank"
 	"moneyconverter/money"
 	"os"
+	"time"
 )
 
 func main() {
@@ -12,18 +14,31 @@ func main() {
 	to := flag.String("to", "EUR", "target currency")
 	flag.Parse()
 
-	fromCurrency, err := money.ParseCurrency(*from)
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "unable to parse source currency %q: %s.\n", *from, err.Error())
-		os.Exit(1)
-	}
-
 	targetCurrency, err := money.ParseCurrency(*to)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "unable to parse target currency %q: %s.\n", *to, err.Error())
 		os.Exit(1)
 	}
+	
+	amount := parseAmount(from)
+	rates :=ecbank.NewClient(30*time.Second)
 
+	convertedAmount, err := money.Convert(amount, targetCurrency, rates)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to convert %s to %s: %s.\n", amount, targetCurrency, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s = %s \n", amount, convertedAmount)
+}
+
+func parseAmount(from *string) money.Amount {
+	fromCurrency, err := money.ParseCurrency(*from)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse source currency %q: %s.\n", *from, err.Error())
+		os.Exit(1)
+	}
+	
 	value := flag.Arg(0)
 	if value == "" {
 		_, _ = fmt.Fprintln(os.Stderr, "missing amount to convert")
@@ -44,11 +59,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	convertedAmount, err := money.Convert(amount, targetCurrency)
-	if err != nil{
-		_, _ = fmt.Fprintf(os.Stderr, "unable to convert %s to %s: %s.\n", amount, targetCurrency, err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Printf("%s = %s \n", amount, convertedAmount)
+	return amount
 }
